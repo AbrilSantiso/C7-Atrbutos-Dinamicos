66160b40659810c5c1a647cdd5dddcb4647365b0
Abril <abrilsantiso@hotmail.com>
2021-06-15 10:52:38 -0300

    Agregando la tercera etapa

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+/* ----FORMA 1-----
 const imagenes = document.querySelectorAll("img");
 const divsTarjetas = document.querySelectorAll(".tarjeta");
-
 divsTarjetas.forEach((divTarjeta, i) => {
   const anchor = document.createElement("a");
   let urlImagen = prompt("Ingrese el url de la imagen " + (i + 1));
@@ -14,10 +14,25 @@ divsTarjetas.forEach((divTarjeta, i) => {
     imagen.setAttribute("src", urlImagen);
     imagen.setAttribute("width", "300px");
   });
-});
+});*/
 
 /* ETAPA 3
 - Nodo padre: div con class contenedor
 - Nodo hijo repetitivo: divs con class tarjeta
-- Explicación: Profe no llegamos a terminar
+- Explicación: Primero obtenemos al div contenedor de todas las tarjetas. Luego
+  cree una funcion que se encargue de pedir la url al usuario, generar la template y agregarlas al nodo padre en
+  cada iteracion. (Salio luego de que otra compañera de otra mesa me ayude)
 */
+
+let nodoPadre = document.querySelector("div");
+
+function crearTemplate() {
+  for (let i = 0; i < 3; i++) {
+    let urlImagen = prompt("Buenas! Introduci la url de la imagen " + (i + 1));
+    let temp = `<div class="tarjeta">
+    <a href=${urlImagen} target=_blank ><img src=${urlImagen} width=300px class="imagen" alt="imagen${i + 1}" id="imagen${i + 1}" /></a>
+  </div>`;
+    nodoPadre.innerHTML += temp;
+  };
+};
+crearTemplate();
